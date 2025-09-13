98ea37683cbb2959afa8461bd522ad7586f65268
Luvi <luviaprilyansyahgabriel@gmail.com>
2025-09-14 02:47:13 +0800

    feat: update environment configuration, add axios and ollama dependencies, and enhance chat service with error handling

--- src/services/chat.services.ts
+++ src/services/chat.services.ts
@@ -1,3 +1,16 @@
-export const generateReply = (message: string) => {
-  return `AI Dummy reply: You said ${message}`;
+import axios from "axios";
+import { OLLAMA_API_URL, MODEL } from "../index";
+
+export const generateReply = async (message: string) => {
+  try {
+    const { data } = await axios.post(`${OLLAMA_API_URL}/api/chat`, {
+      model: MODEL,
+      messages: [{ role: "user", content: message }],
+      stream: false,
+    });
+    return data?.message?.content || "AI not respone";
+  } catch (error) {
+    console.error("Error generating reply:", error);
+    return "Sorry, AI failed to respone.";
+  }
 };
